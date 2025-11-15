14519ec3e9dfea7ea675d7b282373c5f74fb4716
p4blobh <pietrotvkids@gmail.com>
2025-11-15 14:41:42 -0800

    again

--- PieClicker/main.py
+++ PieClicker/main.py
@@ -154,19 +154,19 @@ async def main():
                 run = False
         
         
-        if event.type == pygame.KEYDOWN and nametyping:
-            print("Key pressed")
-            if event.key == pygame.K_RETURN:
-                if len(username) == 3:
+            if event.type == pygame.KEYDOWN and nametyping:
+                print("Key pressed")
+                if event.key == pygame.K_RETURN:
+                    if len(username) == 3:
+                        nametyping=False
+                        open("highscore.txt", "w").write(str(game.mostpies)+" - "+username)
+                elif event.key == pygame.K_BACKSPACE:
+                        username = username[:-1] 
+                elif len(username) < 3 and event.unicode.isalpha():
+                        print("Adding letter")
+                        username += event.unicode.upper()
+                if event.key == pygame.K_ESCAPE:
                     nametyping=False
-                    open("highscore.txt", "w").write(str(game.mostpies)+" - "+username)
-            elif event.key == pygame.K_BACKSPACE:
-                    username = username[:-1] 
-            elif len(username) < 3 and event.unicode.isalpha():
-                    print("Adding letter")
-                    username += event.unicode.upper()
-            if event.key == pygame.K_ESCAPE:
-                nametyping=False
 
         if game.pies > game.mostpies:
             game.mostpies = game.pies
