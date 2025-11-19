85782fec3ff00c3153918f165504ae44983a3b1a
p4blobh <pietrotvkids@gmail.com>
2025-11-18 23:04:09 -0800

    again (why is pygbag like this - filenotfound error just like what originally happened with PieClicker)

--- Minesweeper/main.py
+++ Minesweeper/main.py
@@ -9,7 +9,7 @@ screen_height = 1200
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("Minesweeper")
 
-icon = pygame.image.load("Minesweeper/assets/flag.png")
+icon = pygame.image.load("assets/flag.png")
 pygame.display.set_icon(icon)
 
 clock = pygame.time.Clock()
@@ -17,7 +17,7 @@ clock = pygame.time.Clock()
 #-------------------------------
 #----------- Fonts ------------#
 #-------------------------------
-roboto =("Minesweeper/assets/Roboto.ttf")
+roboto =("assets/Roboto.ttf")
 
 menufont = pygame.font.Font(roboto, 48)
 difffont = pygame.font.Font(roboto, 36)
@@ -32,23 +32,23 @@ medium_text = difffont.render("MEDIUM", True, (0, 0, 0))
 hard_text = difffont.render("HARD", True, (0, 0, 0))
 
 #-------------------------------
-#---------- Minesweeper/assets ------------#
+#---------- assets ------------#
 #-------------------------------
 
-unknown = pygame.image.load("Minesweeper/assets/unknown.png")
-b = pygame.image.load("Minesweeper/assets/b.png")
-b1 = pygame.image.load("Minesweeper/assets/b1.png")
-b2 = pygame.image.load("Minesweeper/assets/b2.png")
-b3 = pygame.image.load("Minesweeper/assets/b3.png")
-b4 = pygame.image.load("Minesweeper/assets/b4.png")
-b5 = pygame.image.load("Minesweeper/assets/b5.png")
-b6 = pygame.image.load("Minesweeper/assets/b6.png")
-b7 = pygame.image.load("Minesweeper/assets/b7.png")
-b8 = pygame.image.load("Minesweeper/assets/b8.png")
-flag = pygame.image.load("Minesweeper/assets/flag.png")
-bomb = pygame.image.load("Minesweeper/assets/bomb.png")
-boom = pygame.image.load("Minesweeper/assets/boom.png")
-wrong = pygame.image.load("Minesweeper/assets/wrong.png")
+unknown = pygame.image.load("assets/unknown.png")
+b = pygame.image.load("assets/b.png")
+b1 = pygame.image.load("assets/b1.png")
+b2 = pygame.image.load("assets/b2.png")
+b3 = pygame.image.load("assets/b3.png")
+b4 = pygame.image.load("assets/b4.png")
+b5 = pygame.image.load("assets/b5.png")
+b6 = pygame.image.load("assets/b6.png")
+b7 = pygame.image.load("assets/b7.png")
+b8 = pygame.image.load("assets/b8.png")
+flag = pygame.image.load("assets/flag.png")
+bomb = pygame.image.load("assets/bomb.png")
+boom = pygame.image.load("assets/boom.png")
+wrong = pygame.image.load("assets/wrong.png")
 
 #-------------------------------
 #----------- Set Up -----------#
